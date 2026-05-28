2f0696673f3a91576007b722198990e9a3c0f872
Debbido27 <davidmedina272004@gmail.com>
2026-05-28 15:11:36 -0600

    Creacion de variales y de metodo

---
+++ src/prueba2/pkg22141094/logic.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba2.pkg22141094;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Dell
+ */
+public class logic {
+    private File file = null;
+    private int txtCount =0;
+    private int javaCount=0;
+    private int pdfCount=0;
+    private int otherCount=0;
+    
+    
+    private List<String> resultados = new ArrayList<>();
+    
+    
+    public void setFile (String ruta){
+        file=new File(ruta);
+    }
+    
+    
+    public File getFile(){
+        return file;
+    }
+}
